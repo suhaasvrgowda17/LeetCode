08cfcabf520d93a5b8122730bfc7848e59b90a83
suhaasvrgowda17 <vrsuhaasgowda17@gmail.com>
2026-06-17 08:03:41 +0530

    Add Two Sum solution

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,23 @@
+/*
+
+Probelm Name: "Two Sum"
+Logic:
+1. Check every possible pair.
+2. If the pair sum equals target, return their indices.
+
+Time Complexity: O(n²)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int[] twoSum(int[] nums,int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                // Valid pair found
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
